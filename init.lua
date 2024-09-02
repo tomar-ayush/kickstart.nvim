2bb49c7c025b8ad660f83ce60857088c8402dc5d
Ayush <ayushtomar8624@gmail.com>
2024-09-02 20:12:49 +0530

    changes in debug function and added custom keymap to run a program in java and lua

--- init.lua
+++ init.lua
@@ -176,19 +176,19 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -775,7 +775,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<CR>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -3,13 +3,30 @@
 -- Exit insert mode with 'jk'
 vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
 
--- Move to the next character in insert mode
--- vim.keymap.set('i', '<C-l>', '<ESC>la', { noremap = true, silent = true })
--- vim.keymap.set('i', '<C-k>', '<ESC>ka', { noremap = true, silent = true })
--- vim.keymap.set('i', '<C-j>', '<ESC>ja', { noremap = true, silent = true })
--- vim.keymap.set('i', '<C-h>', '<ESC>ha', { noremap = true, silent = true })
+--vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>')
+-- vim.keymap.set('n', '<leader>th', ':Telescope colorscheme<CR>')
+--vim.keymap.set('n', '<leader>of', ':Telescope old_files<CR>')
+--vim.keymap.set('n', '<leader>fw', ':Telescope live_grep<CR>')
+
+-- Exit insert mode and move left, down, up, or right
+vim.keymap.set('i', '<C-;>', '<ESC>la', { desc = 'Exit insert mode and move right' })
+vim.keymap.set('i', '<C-k>', '<ESC>ka', { desc = 'Exit insert mode and move up' })
+vim.keymap.set('i', '<C-j>', '<ESC>ja', { desc = 'Exit insert mode and move down' })
+vim.keymap.set('i', '<C-p>', '<ESC>ha', { desc = 'Exit insert mode and move left' })
 
 -- Map Ctrl + A in insert mode to move to the end of the line and enter insert mode
-vim.api.nvim_set_keymap('i', '<C-a>', '<Esc>$a', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-a>', '<Esc>$a', { noremap = true, silent = true })
+-- Keymaps to run a Program
+vim.keymap.set('n', '<leader>rn', function()
+  local file_name = vim.api.nvim_buf_get_name(0)
+  local file_type = vim.bo.filetype
+
+  if file_type == 'lua' then
+    vim.cmd(':tabnew | terminal lua ' .. file_name)
+  elseif file_type == 'java' then
+    local class_name = vim.fn.fnamemodify(file_name, ':t:r')
+    vim.cmd(':tabnew | terminal bash -c "javac ' .. file_name .. ' && java ' .. class_name .. '"')
+  end
+end)
 
 return {}
